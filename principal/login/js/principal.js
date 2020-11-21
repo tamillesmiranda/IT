4416d919d7b35b4a8de83815be810beb8100bcc7
tamillesmiranda <48859615+tamillesmiranda@users.noreply.github.com>
2020-11-20 21:42:45 -0300

    ghgh

---
+++ principal/login/js/principal.js
@@ -0,0 +1,41 @@
+function redirecionar(){
+    window.location.href = "file:///C:/Users/user/Documents/GitHub/IT/principal/novo/novo.html";
+}
+/*function pulaLinha() {
+
+    document.write("<br>");
+}
+
+function mostra(frase) {
+
+    document.write(frase);
+    pulaLinha();
+}
+
+var loginCadastrado = "teste";
+var senhaCadastrada = "teste";
+
+var tentativas = 1;
+
+while (tentativas <= 3) {
+
+     loginInformado = prompt("Informe seu login");
+     senhaInformada = prompt("Informe sua senha");
+
+
+    if( loginCadastrado == loginInformado && senhaCadastrada == senhaInformada ) {
+
+        mostra("Bem-vindo ao sistema " + loginInformado);
+
+
+        break; // Para sair do loop
+
+    } else {
+
+        alert("Login inválido. Tente novamente");
+        tentativas++;
+
+    }
+
+
+}*/
